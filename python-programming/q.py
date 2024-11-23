554525c1a4f2ea35212d49196445fd07862ae9f0
phlusupanat <164610816+phlusup@users.noreply.github.com>
2024-11-23 09:36:45 +0000

    Add program for score rank

--- python-programming/q.py
+++ python-programming/q.py
@@ -1,6 +1,8 @@
 #Hello Everyone
-i = 0
-while i < 4:
-    print("Hello")
-    i += 1
-print("dfd")
+score  = float(input())
+if score > 80:
+    print("Passed")
+elif score > 50:
+    print("Good")
+else:
+    print("Failed")
