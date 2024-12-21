7fe4016148cc0e2b5c95b42a627c99be9b4833da
supanat <supanatkritsana@gmail.com>
2024-12-21 19:51:57 +0700

    update

--- c-programming/Introduction.c
+++ c-programming/Introduction.c
@@ -1,4 +1,15 @@
-#include <studio.h>
-main (
-    printf("Hello")
-)
+
+/*
+#include<stdio.h>
+#define PI 3.14159
+int main()
+{
+    float r,a;
+    printf("Enter radius : ");
+    scanf("%f", &r);
+    a = PI * r * r;
+    printf("Area : %.2f\n", a);
+    return 0;
+}
+*/
+
