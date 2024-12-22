6a0dcd1631a4db294696f00630841d26c0a5b6a7
supanat <supanatkritsana@gmail.com>
2024-12-22 19:43:31 +0700

    update

--- c-programming/Introduction.c
+++ c-programming/Introduction.c
@@ -106,3 +106,38 @@ int main()
     printf("Area = %d\n", a);
 }
 */
+/*
+#include<stdio.h>
+int main()
+{
+    float x, a = 5, b = 2;
+    x = a/b;
+    printf("x = %f\n", x);
+}
+
+*/
+
+/*
+#include<stdio.h>
+int main()
+{
+    float x;
+    int a = 5, b = 2;
+    x = a/b;
+    printf("x = %f\n", x);
+}
+
+*/
+
+
+/*
+#include<stdio.h>
+int main()
+{
+    int sum = 22, count = 5;
+    float mean;
+    mean = (float) sum/count;
+    printf("Mean = %f\n", mean);
+}
+*/
+
