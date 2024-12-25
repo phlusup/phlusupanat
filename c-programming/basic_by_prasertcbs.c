ee96622694fe417592de4f2ecd88568340154801
supanat <supanatkritsana@gmail.com>
2024-12-25 17:52:03 +0700

    update

---
+++ c-programming/basic_by_prasertcbs.c
@@ -0,0 +1,47 @@
+// Basic C Programming
+/*
+#include<stdio.h>
+int main()
+{
+    printf("hello world\n");
+    printf("don't worry by happy.\n");
+
+}
+*/
+
+
+/*
+#include<stdio.h>
+int main()
+{
+    int area;
+    int w, h;
+    printf("Enter width = ");
+    scanf("%d", &w); //Input
+    printf("Enter height = ");
+    scanf("%d", &h);
+    area = w * h; // Process
+    printf("Rectangle area = %d\n", area); // Output
+}
+*/
+
+/*
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    printf("%d\n", 5 + 3);
+    printf("%d\n", 5 - 3);
+    printf("%d\n", 5 * 3);
+    printf("%d\n", 5 / 3);
+    printf("%f\n", 5.0 / 3.0);
+    printf("%f\n", 5.0 / 3);
+    printf("%f\n", 5 / 3.0);
+    printf("%f\n", 5.0 + 3 * 2 - 4);
+    printf("%f\n", 5.0 + 6 - 4/2 - 1*9);
+    printf("%f\n", -5.0 + 6 - 4/2 - 1*9);
+    printf("%f\n", pow(2,3));
+    // int vs double/float
+}
+*/
+
