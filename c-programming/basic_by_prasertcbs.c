c69a3b9fe0da2f66b4c5829613fa12364c668ca8
supanat <supanatkritsana@gmail.com>
2024-12-26 13:30:29 +0700

    update

--- c-programming/basic_by_prasertcbs.c
+++ c-programming/basic_by_prasertcbs.c
@@ -244,7 +244,7 @@ int main(){
 }
 */
 
-
+/*
 #include<stdio.h>
 void demo_int(){
     int n = 84, m = 43;
@@ -270,3 +270,34 @@ int main() {
     demo_char();
     return 0;
 }
+*/
+
+/*
+#include<stdio.h>
+#include<string.h>
+void demo_str() {
+    char *str = "rainbow";
+    printf("str = |%s|\n", str);
+    printf("str = |%10s|\n", str);
+    printf("str = |%-10s|\n", str);
+    printf("str = |%.4s|\n", str);
+    printf("str = |%.*s|\n", 4,str);
+}
+void demo_addr() {
+    int i  = 10;
+    printf("i = %d (%p)\n", i, &i);
+    char *str = "rainbow";
+    char str2[] = "sunday";
+    printf("str = %s (%p)\n", str, str);
+    printf("str2 = %s (%p)\n", str2, str2);
+    for(int i = 0; i < strlen(str2); i++){
+        printf("str2[%d] = %c (%p) \n", i, str2[i], &str2[i]);
+    }
+}
+int main(){
+    demo_str();
+//    demo_addr();
+    return 0;  
+}
+*/
+
