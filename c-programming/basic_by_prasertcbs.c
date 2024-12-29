268a265c0a2e72135689658a30d4004875ed7162
supanat <supanatkritsana@gmail.com>
2024-12-29 18:37:36 +0700

    update

--- c-programming/basic_by_prasertcbs.c
+++ c-programming/basic_by_prasertcbs.c
@@ -710,3 +710,23 @@ int main(){
 }
 */
 
+#include<stdio.h>
+void asciiTable() {
+    for(int i = 0; i < 256; i++){
+        printf("%d: %c\n",i, i );
+    }
+}
+void demo1() {
+    char c1;
+    c1 = 'A';
+    char c2 = 'B';
+    char c3;
+    c3 = 'A'+ 2;
+    printf("%c %d\n", c1, c1);
+    printf("%c\n", c2);
+    printf("%c\n", c3);
+}
+
+int main() {
+    asciiTable();
+}
