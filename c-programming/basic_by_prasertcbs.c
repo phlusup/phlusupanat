f3d6786caa6bcf58da3cb45f438300766de23525
supanat <supanatkritsana@gmail.com>
2024-12-29 12:29:11 +0700

    update

--- c-programming/basic_by_prasertcbs.c
+++ c-programming/basic_by_prasertcbs.c
@@ -356,9 +356,9 @@ int main()
 }
 */
 
+/*
 #include<stdio.h>
-int main() 
-{
+void demo() {
     char d;
     printf("[m]ocha\n");
     printf("[l]atte\n");
@@ -378,3 +378,60 @@ int main()
         printf("Please select a valid menu.\n");
     }
 }
+void demo2() {
+    int age;
+    int ticket;
+    printf("Enter your age : ");
+    scanf(" %d", &age);
+    if (age < 5 || age >= 60) {
+        ticket = 0;
+    } else {
+        ticket = 50;
+    }
+    printf("Ticket = %d\n", ticket);
+}
+
+
+int main() {
+    demo2();
+}
+*/
+
+/*
+
+#include<stdio.h>
+void demo1() {
+    int a = 10, b = 27;
+    int max;
+    // if (a>b){
+    //     max = a;
+    // } else {
+    //     max = b;
+    // }
+    max = a > b ? a : b;
+    printf("max = %d\n", max);
+}
+
+int max(int a, int b){
+    return a > b ? a : b;
+}
+
+void demo2(){
+    int age = 40;
+    int ticket;
+    // if (age < 5 || age > 60){
+    //     ticket = 0;
+    // } else {
+    //     ticket = 50;
+    // }
+    //ternary operator
+    // ticket = age < 5 || age > 50 ? 0 : 50;
+    // printf("ticket = %d\n", ticket);
+    age < 5 || age > 50 ? printf("ticket = 0\n") : printf("ticket = 50\n");
+}
+
+int main(){
+    demo2();
+}
+*/
+
