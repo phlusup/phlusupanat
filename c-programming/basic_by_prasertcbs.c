7f332177d9dc4714b6a3477b101cee87f3280b0f
supanat <supanatkritsana@gmail.com>
2024-12-29 16:02:02 +0700

    update

--- c-programming/basic_by_prasertcbs.c
+++ c-programming/basic_by_prasertcbs.c
@@ -662,11 +662,9 @@ int main() {
     int a = 9, b = 21;
     printf("gcd(%d, %d) = %d\n", a,b, gcd(a, b));
 }
-
-
+*/
 
 /*
-
 #include<stdio.h>
 #include<math.h>
 int gcdEuclid(int a, int b){
@@ -684,3 +682,31 @@ int main(){
     printf("gcd(%d, %d) = %d\n", a,b,gcdEuclid(a,b));
 }   
 */
+
+
+// babylonian method 
+
+/*
+#include<stdio.h>
+#include<math.h>
+double sqrtt(double s, double precision){
+    double x0 = 600;
+    double x1;
+    //double precision = .0001;
+    double delta = 1; //must more than precision
+    while (delta > precision) {
+        x1 = .5 * (x0 + s/x0);
+        delta = fabs(x1 -x0);
+        printf("x0 = %.7f, x1 = %.7f, delta = %.7f\n", x0, x1, delta);
+        x0 = x1;
+    }
+    return x1;
+}
+int main(){
+    double s = 125348;
+    double x = sqrtt(s, 0.0000001);
+    printf("x = %.7f, x * x = %.7f\n", x, x * x);
+    return 0;
+}
+*/
+
