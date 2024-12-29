cb4c90a8663b619f8f123b925284004434102c39
supanat <supanatkritsana@gmail.com>
2024-12-29 15:06:00 +0700

    update

--- c-programming/basic_by_prasertcbs.c
+++ c-programming/basic_by_prasertcbs.c
@@ -614,3 +614,32 @@ int main() {
 }
 */
 
+/*
+#include<stdio.h>
+#include<math.h>
+int saving(double pv, double rate, double fv){
+    int i = 1;
+    double amt = 0;
+    do {
+        amt = pv * pow(1+rate, i);
+        printf("year %d, amt = %.2f\n", i, amt);
+        i++;
+
+    }while( amt <= fv );
+    return --i;
+}
+int saving2(double pv, double rate, double fv){
+    int i = 1;
+    double amt = 0;
+    do {
+        amt = pv * pow(1+rate, i++);
+        printf("year %d, amt = %.2f\n", i, amt);
+
+    }while( amt <= fv );
+    return --i;
+}
+int main(){
+    printf("year = %d\n", saving(1000, .05, 2000));
+    printf("year = %d\n", saving2(1000, .05, 2000));
+}
+*/
