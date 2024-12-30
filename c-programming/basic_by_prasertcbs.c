49db981b1970439adf670a666bf034774c44894f
supanat <supanatkritsana@gmail.com>
2024-12-30 13:36:44 +0700

    update

--- c-programming/basic_by_prasertcbs.c
+++ c-programming/basic_by_prasertcbs.c
@@ -761,3 +761,31 @@ int main() {
 }
 */
 
+
+/*
+#include<stdio.h>
+#include<stdbool.h>
+
+bool leapYear(int year) {
+    // int r;
+    // r = year % 4;
+    // if( r==0 ) {
+    //     return true; 
+    // } else {
+    //     return false;
+    // }
+    return year % 4 == 0 ? true : false;
+}
+bool isOdd(int n){
+    return n % 2 == 1? true : false;
+}
+bool isEven(int n ){
+    //return n % 2 == 0? true : false;
+    return !isOdd(n);
+}
+int main() {
+    int isLeapYear = leapYear(2016);
+    printf("%d\n", isLeapYear);
+}
+*/
+
