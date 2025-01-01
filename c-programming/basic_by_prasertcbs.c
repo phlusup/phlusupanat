fe6201ddddb70815916e765180422c0ce68b002e
supanat <supanatkritsana@gmail.com>
2025-01-01 11:25:54 +0700

    update

--- c-programming/basic_by_prasertcbs.c
+++ c-programming/basic_by_prasertcbs.c
@@ -1000,7 +1000,36 @@ int main() {
 }
 */
 
+//การเขียนฟังก์ชันที่ส่งค่ากลับมามากกว่าหนึ่งค่า ส่งค่ากลับมาเป็น array ก็ได้
 #include<stdio.h>
+
+typedef struct 
+{
+    double price;
+    double vat;
+} PriceType;
+
+
+PriceType pricePart2(double totalPrice) {
+    PriceType p;
+    p.vat = totalPrice * 7.0 / 107;
+    p.price = totalPrice - p.vat;
+    return p;
+}
+
+void pricePart(double totalPrice, double *price, double *vat) {
+    *vat = totalPrice * 7.0 / 107;
+    *price = totalPrice - *vat;
+
+}
 int main() {
+    //total price = 107, price = 100, VAT = 7
+    double price, vat, totalPrice;
+    totalPrice = 107;
+    pricePart(totalPrice, &price, &vat);
+    printf("total price = %.2f, price = %.2f, vat = %.2f\n", totalPrice, price, vat);
+    PriceType p = pricePart2(totalPrice);
+    printf("total price = %.2f, price = %.2f, vat = %.2f\n", totalPrice, p.price, p.vat);
+
     return 0;
 }
